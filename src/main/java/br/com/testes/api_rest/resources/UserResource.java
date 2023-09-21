df4f77ec8d6c4c7b8df31d5ec4da06e482132972
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-09-21 16:14:46 -0300

    inserindo padrão DTO

--- src/main/java/br/com/testes/api_rest/resources/UserResource.java
+++ src/main/java/br/com/testes/api_rest/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.testes.api_rest.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.testes.api_rest.entities.User;
+import br.com.testes.api_rest.entities.dto.UserDTO;
 import br.com.testes.api_rest.services.UserService;
 
 @RestController
@@ -17,11 +19,14 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<User>findById(@PathVariable Integer id){
+	public ResponseEntity<UserDTO>findById(@PathVariable Integer id){
 		User user= service.findById(id);
 		
-		return ResponseEntity.ok().body(user);
+		return ResponseEntity.ok().body(modelMapper.map(user, UserDTO.class));
 	}
 
 }
