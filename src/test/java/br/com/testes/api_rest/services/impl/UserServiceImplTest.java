f1f65dac5bdf593ea9ebb5a8472eb9539c0d6c19
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-09-22 14:14:40 -0300

    teste para classe UserService método findById

---
+++ src/test/java/br/com/testes/api_rest/services/impl/UserServiceImplTest.java
@@ -0,0 +1,102 @@
+package br.com.testes.api_rest.services.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.testes.api_rest.entities.User;
+import br.com.testes.api_rest.entities.dto.UserDTO;
+import br.com.testes.api_rest.repositories.UserRepository;
+
+@SpringBootTest
+class UserServiceImplTest {
+	
+	private static final String SENHA = "poscrise";
+
+	private static final String EMAIL = "coringuete@gmail.com";
+
+	private static final String NAME = "Bruce Wayne";
+
+	private static final Integer ID = 1;
+
+	@InjectMocks
+	private UserServiceImpl service;
+	
+	@Mock
+	private UserRepository repository;
+	
+	@Mock
+	private ModelMapper mapper;
+	
+	private User user;
+	private UserDTO userDTO;
+	
+	private Optional<User>optionalUser;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+		startUser();
+	}
+
+	@Test
+	void testFindById() {
+		when(repository.findById(anyInt())).thenReturn(optionalUser);
+		
+		User response= service.findById(ID);
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(ID, response.getId());
+		assertEquals(ID, response.getId());
+		assertEquals(ID, response.getId());
+		assertEquals(ID, response.getId());
+		
+	}
+
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+	private void startUser() {
+		user= new User(ID, NAME, EMAIL, SENHA);
+		userDTO= new UserDTO(1, NAME, EMAIL, SENHA);
+		optionalUser= Optional.of(new User(1, NAME, EMAIL, SENHA));
+	}
+
+}
